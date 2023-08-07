b39d5d2eac270232035defcee4db4ac3bec8368c
Nisal Tharuka <127920705+Murphycoding@users.noreply.github.com>
2023-08-07 15:58:58 +0530

    Initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,57 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        color1: "#D7573B",
+        color2: "#FA9C0F",
+        color3: "#152F37",
+        color4: "#35528B",
+      },
+      fontFamily: {
+        poppins: ["Poppins"],
+      },
+      fontSize: {
+        14: '14px',
+      },
+      backgroundColor: {
+        'main-bg': '#FAFBFB',
+        'main-dark-bg': '#20232A',
+        'secondary-dark-bg': '#33373E',
+        'light-gray': '#F7F7F7',
+        'half-transparent': 'rgba(0, 0, 0, 0.5)',
+      },
+      borderWidth: {
+        1: '1px',
+      },
+      borderColor: {
+        color: 'rgba(0, 0, 0, 0.1)',
+      },
+      width: {
+        400: '400px',
+        760: '760px',
+        780: '780px',
+        800: '800px',
+        1000: '1000px',
+        1200: '1200px',
+        1400: '1400px',
+      },
+      height: {
+        80: '80px',
+      },
+      minHeight: {
+        590: '590px',
+      },
+      backgroundImage: {
+        'hero-pattern':
+          "url('https://i.ibb.co/MkvLDfb/Rectangle-4389.png')",
+      },
+    },
+  },
+  plugins: [],
+}
+
