90ac39a153ec43cf7d6ace98d618da67d66e46d6
Nisal Tharuka <127920705+Murphycoding@users.noreply.github.com>
2023-08-20 01:40:33 +0530

    create consultant-signup

--- src/pages/consunltant/ConsultantSignup.jsx
+++ src/pages/consunltant/ConsultantSignup.jsx
@@ -1,8 +1,66 @@
-import React from 'react';
-const ConsultantSignup = () => {
+import { Link } from "react-router-dom";
+const Consunltantsignup = () => {
     return (
-        <div>Consunltantsignup</div>
+    <div class="container mx-auto p-8 mt-36 flex">
+        <div class="max-w-md w-full mx-auto">
+         
+            <div class="bg-white rounded-lg overflow-hidden shadow-2xl">
+                <div class="p-8">
+                <div className=" h3  text-center">Consunltants</div>
+                    <form method="POST" class="" action="#" onSubmit="return false;">
+                
+                        <div class="mb-5">
+                            <label for="name" class="block mb-2 text-sm font-medium text-gray-600">Full Name</label>
+
+                            <input type="text" name="name" class="block w-full p-3 rounded bg-gray-200 border border-transparent focus:outline-none"/>
+                        </div>
+                        <div class="mb-5">
+                            <label for="contactnumber" class="block mb-2 text-sm font-medium text-gray-600">Contact Number</label>
+
+                            <input type="text" name="contactnumber" class="block w-full p-3 rounded bg-gray-200 border border-transparent focus:outline-none"/>
+                        </div>
+                        <div class="mb-5">
+                            <label for="addres" class="block mb-2 text-sm font-medium text-gray-600">Addres</label>
+
+                            <input type="text" name="addres" class="block w-full p-3 rounded bg-gray-200 border border-transparent focus:outline-none"/>
+                        </div>
+        
+                        <div class="mb-5">
+                            <label for="age" class="block mb-2 text-sm font-medium text-gray-600">Age</label>
+
+                            <input type="text" name="age" class="block w-full p-3 rounded bg-gray-200 border border-transparent focus:outline-none"/>
+                        </div>
+                        <div class="mb-5">
+                            <label for="gender" class="block mb-2 text-sm font-medium text-gray-600">Gender</label>
+
+                            <input type="text" name="gender" class="block w-full p-3 rounded bg-gray-200 border border-transparent focus:outline-none"/>
+                        </div>
+                        <div class="mb-5">
+                            <label for="email" class="block mb-2 text-sm font-medium text-gray-600">Email</label>
+
+                            <input type="text" name="email" class="block w-full p-3 rounded bg-gray-200 border border-transparent focus:outline-none"/>
+                        </div>
+                
+                        <div class="mb-5">
+                            <label for="password" class="block mb-2 text-sm font-medium text-gray-600">Password</label>
+
+                            <input type="text" name="password" class="block w-full p-3 rounded bg-gray-200 border border-transparent focus:outline-none"/>
+                        </div>
+
+                        <button class="w-full p-3 mt-4 bg-color2 text-white rounded shadow">Signup</button>
+                    </form>
+                </div>
+                <div class="flex justify-between p-8 text-sm border-t border-gray-300 bg-gray-100">
+                 
+                    <Link to="/Job_seekersignup" className="text-gray-600">Job Seeker</Link>
+                    <Link to="/Consunltantsignup" className="text-gray-600">Consunltant</Link>
+                    <Link to="/Signup" className="text-gray-600">Admin</Link>
+             </div> 
+        
+            </div>
+        </div>
+    </div>
     )
 }
 
-export default ConsultantSignup
+export default Consunltantsignup
