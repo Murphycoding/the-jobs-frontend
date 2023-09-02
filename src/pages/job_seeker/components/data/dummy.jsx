69b32affc6047008932fe98908efe00cced9cafb
Nisal Tharuka <127920705+Murphycoding@users.noreply.github.com>
2023-09-02 21:31:16 +0530

    job-seeker slide bar

--- src/pages/job_seeker/components/data/dummy.jsx
+++ src/pages/job_seeker/components/data/dummy.jsx
@@ -477,86 +477,14 @@ export const links = [
     title: 'Dashboard',
     links: [
       {
-        name: 'ecommerce',
-        icon: <FiShoppingBag />,
-      },
-    ],
-  },
-
-  {
-    title: 'Pages',
-    links: [
-      {
-        name: 'orders',
-        icon: <AiOutlineShoppingCart />,
-      },
-      {
-        name: 'employees',
+        name: 'View Consultants',
         icon: <IoMdContacts />,
       },
       {
-        name: 'customers',
+        name:'Get Appointment',
         icon: <RiContactsLine />,
       },
-    ],
-  },
-  {
-    title: 'Apps',
-    links: [
-      {
-        name: 'calendar',
-        icon: <AiOutlineCalendar />,
-      },
-      {
-        name: 'kanban',
-        icon: <BsKanban />,
-      },
-      {
-        name: 'editor',
-        icon: <FiEdit />,
-      },
-      {
-        name: 'color-picker',
-        icon: <BiColorFill />,
-      },
-    ],
-  },
-  {
-    title: 'Charts',
-    links: [
-      {
-        name: 'line',
-        icon: <AiOutlineStock />,
-      },
-      {
-        name: 'area',
-        icon: <AiOutlineAreaChart />,
-      },
-
-      {
-        name: 'bar',
-        icon: <AiOutlineBarChart />,
-      },
-      // {
-      //   name: 'pie',
-      //   icon: <FiPieChart />,
-      // },
-      // {
-      //   name: 'financial',
-      //   icon: <RiStockLine />,
-      // },
-      // {
-      //   name: 'color-mapping',
-      //   icon: <BsBarChart />,
-      // },
-      // {
-      //   name: 'pyramid',
-      //   icon: <GiLouvrePyramid />,
-      // },
-      // {
-      //   name: 'stacked',
-      //   icon: <AiOutlineBarChart />,
-      // },
+      
     ],
   },
 ];
