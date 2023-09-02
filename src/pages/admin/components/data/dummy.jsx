c6ebda8d0bb7493c39352529ee0077fefd2f2eac
Nisal Tharuka <127920705+Murphycoding@users.noreply.github.com>
2023-09-02 16:41:27 +0530

    job-seeker dashboard

--- src/pages/admin/components/data/dummy.jsx
+++ src/pages/admin/components/data/dummy.jsx
@@ -475,90 +475,14 @@ export const employeesGrid = [
 export const links = [
   {
     title: 'Dashboard',
-    links: [
-      {
-        name: 'ecommerce',
-        icon: <FiShoppingBag />,
-      },
+    links: [ {
+      name: 'Manage Accounts ',
+      icon: <RiContactsLine />,
+    }
+      
     ],
   },
 
-  {
-    title: 'Pages',
-    links: [
-      {
-        name: 'orders',
-        icon: <AiOutlineShoppingCart />,
-      },
-      {
-        name: 'employees',
-        icon: <IoMdContacts />,
-      },
-      {
-        name: 'customers',
-        icon: <RiContactsLine />,
-      },
-    ],
-  },
-  {
-    title: 'Apps',
-    links: [
-      {
-        name: 'calendar',
-        icon: <AiOutlineCalendar />,
-      },
-      {
-        name: 'kanban',
-        icon: <BsKanban />,
-      },
-      {
-        name: 'editor',
-        icon: <FiEdit />,
-      },
-      {
-        name: 'color-picker',
-        icon: <BiColorFill />,
-      },
-    ],
-  },
-  {
-    title: 'Charts',
-    links: [
-      {
-        name: 'line',
-        icon: <AiOutlineStock />,
-      },
-      {
-        name: 'area',
-        icon: <AiOutlineAreaChart />,
-      },
-
-      {
-        name: 'bar',
-        icon: <AiOutlineBarChart />,
-      },
-      // {
-      //   name: 'pie',
-      //   icon: <FiPieChart />,
-      // },
-      // {
-      //   name: 'financial',
-      //   icon: <RiStockLine />,
-      // },
-      // {
-      //   name: 'color-mapping',
-      //   icon: <BsBarChart />,
-      // },
-      // {
-      //   name: 'pyramid',
-      //   icon: <GiLouvrePyramid />,
-      // },
-      // {
-      //   name: 'stacked',
-      //   icon: <AiOutlineBarChart />,
-      // },
-    ],
-  },
 ];
 
 export const cartData = [
